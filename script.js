537d985583389efaf16fc13bb176fee8d3ef4415
lamachin-dev <56517440+lamachin-dev@users.noreply.github.com>
2024-02-03 04:18:54 -0800

    feat: function to play 1 round, keep score

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-console.log("test");
+
+const getComputerChoice = function(){
+    const randNum = Math.floor(Math.random()*3);
+    let computerChoice = "";
+    
+    if (randNum === 2){computerChoice = "rock"};
+    if (randNum === 1){computerChoice = "paper"};
+    if (randNum === 0){computerChoice = "scissors"};
+    return computerChoice;
+}
+
+
+const playRound = function(){
+    const playerSelection = prompt("type your choice:").toLowerCase()
+    const computerSelection = getComputerChoice()
+    console.log("player:", playerSelection," vs ", "pc:", computerSelection);
+
+    let winner ="";    
+    //ties
+    if(playerSelection === computerSelection){
+        winner = "tie"
+        return winner
+    }
+
+    //player win
+    if(
+        playerSelection === "rock" && computerSelection === "scissors"
+        || playerSelection === "scissors" && computerSelection === "paper"
+        || playerSelection === "paper" && computerSelection === "rock"
+    ){
+        winner = "player";
+        return winner
+    }
+    //rest of cases, computer wins
+    else{
+        winner = "computer";
+        return winner
+    }
+}
+
+
+const playGame = function(){
+    let playerScore = 0;
+    let computerScore = 0;
+    let round = 0;
+
+    for(let i = 0; i < 5; i++){
+        //Increase score
+        if(playRound() === "player"){playerScore++}
+        else if(playRound() === "computer") {computerScore++}
+        round++;
+        console.log(`Round(${round}) - Player:${playerScore} Computer:${computerScore}`);
+    }
+
+}
+
+playGame()
